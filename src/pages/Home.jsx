8eef8cbb24315dfdf850f0b0439fb7884e21b44c
vj146 <121240553+vj146@users.noreply.github.com>
2024-09-02 10:48:44 +0100

    responsive check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -208,7 +208,7 @@ export default function Home() {
         <ColorSwatch />
       </section>
 
-      <section className="md:container md:flex mx-auto mb-20">
+      <section className="md:container md:flex mx-auto md:mb-20">
         <div className="flex flex-1 items-end bg-cover bg-center bg-no-repeat text-center" 
           style={{ 
             backgroundImage: `url(${BD_selfcare})` 
